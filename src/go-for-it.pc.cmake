bdf9ace45984cf981154e0751117e1943030efd3
Jonathan <jonathanmoerman@gmail.com>
2015-12-23 17:12:54 +0100

    Update CMakeList.txt according to the changes, install headers and vapi.

---
+++ src/go-for-it.pc.cmake
@@ -0,0 +1,11 @@
+prefix=@PREFIX@
+exec_prefix=@DOLLAR@{prefix}
+libdir=@DOLLAR@{prefix}/lib
+includedir=@DOLLAR@{prefix}/include
+ 
+Name: Go For It!
+Description: Go For It! headers
+Version: @VERSION@
+Cflags: -I@DOLLAR@{includedir}/go-for-it
+Requires: gtk+-3.0 libpeas-1.0
+
